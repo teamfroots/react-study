a70a755beb98d854d000058132b518f80f4d5f7a
dasoml <somgrapik@gmail.com>
2020-08-02 15:39:40 +0900

    태그 사이의 내용을 보여주는 children 값을 보여주는 연습

--- 03/src/App.js
+++ 03/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
+  return (
+    <MyComponent>
+      리액트 {/*태그 사이의 내용을 보여주는 children 값*/}
+    </MyComponent>
+  );
 };
 
 export default App;
--- 03/src/MyComponent.js
+++ 03/src/MyComponent.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const MyComponent = (props) => {
-  return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
+  return (
+    <div>
+      안녕하세요, 제 이름은 {props.name}입니다. <br />
+      children 값은 {props.children} 입니다.
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {
-    name : "땡땡땡"
-}
+  name: "땡땡땡",
+};
 
 export default MyComponent;
