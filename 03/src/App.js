83c8fa944f130bc94aa9f96ffe389d9a740c88d7
dasoml <somgrapik@gmail.com>
2020-08-02 15:03:49 +0900

    클래스형 컴포넌트 사용 연습

--- 03/src/App.js
+++ 03/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const name = "리액트";
+//   return (
+//     <>
+//       <div>{name}</div>
+//     </>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    const name2 = "뤼액트";
+    return <div>{name2}</div>;
+  }
 }
 
 export default App;
