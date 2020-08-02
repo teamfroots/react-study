a12b7beac9723066dd16c76f5b7576de17beb8b0
dasoml <somgrapik@gmail.com>
2020-08-02 16:08:34 +0900

    클래스형 컴포넌트에서 props 연습

--- 03/src/MyComponent.js
+++ 03/src/MyComponent.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      안녕하세요, 제 이름은 {name}입니다. <br />
-      제가 좋아하는 숫자는 {favoriteNumber}입니다.
-      <br />
-      children 값은 {children} 입니다.
-    </div>
-  );
-};
+// const MyComponent = ({ name, favoriteNumber, children }) => {
+//   return (
+//     <div>
+//       안녕하세요, 제 이름은 {name}입니다. <br />
+//       제가 좋아하는 숫자는 {favoriteNumber}입니다.
+//       <br />
+//       children 값은 {children} 입니다.
+//     </div>
+//   );
+// };
 
-MyComponent.defaultProps = {
-  name: "땡땡땡",
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: "땡땡땡",
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name}입니다. <br />
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
+        <br />
+        children 값은 {children} 입니다.
+      </div>
+    );
+  }
+}
 
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired,
-};
+// MyComponent.defaultProps = {
+//   name: "땡땡땡",
+// };
+
+// MyComponent.propTypes = {
+//   name: PropTypes.string,
+//   favoriteNumber: PropTypes.number.isRequired,
+// };
 
 export default MyComponent;
