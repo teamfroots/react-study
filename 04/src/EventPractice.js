fde6cc04054a8129035ab6364361a2e0d8ba127f
dasoml <somgrapik@gmail.com>
2020-08-03 22:15:06 +0900

    input 여러개 다루기

--- 04/src/EventPractice.js
+++ 04/src/EventPractice.js
@@ -2,21 +2,22 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     welcome: "",
   };
 
   /* 훨씬 깔끔하게 정리한 방법. 화살표 함수 형태로 메서드 정의하기 */
   handleChange = (e) => {
     this.setState({
-      welcome: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   handleClick = () => {
-    this.setState({ welcome: "" });
+    this.setState({ welcome: "", username: "" });
     console.log(this.state.welcome);
     const h2 = document.querySelector("h2");
-    h2.innerText = this.state.welcome;
+    h2.innerText = this.state.username + " : " + this.state.welcome;
   };
 
   render() {
@@ -24,12 +25,22 @@ class EventPractice extends Component {
       <div>
         <h1>이벤트 연습</h1>
         <h2></h2>
+        name:
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        ></input>
+        <br />
+        message:
         <input
           type="text"
           name="welcome"
           value={this.state.welcome}
           onChange={this.handleChange}
         ></input>
+        <br />
         <button onClick={this.handleClick}>enter</button>
       </div>
     );
