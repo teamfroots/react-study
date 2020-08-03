912ec66309c6a8a2e2a531a250e8d0dc7fab4c25
dasoml <somgrapik@gmail.com>
2020-08-03 21:20:12 +0900

    state에 input 값 담고 버튼을 누르면 input 값 알림창띄워 보여주기 연습

---
+++ 04/src/EventPractice.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요"
+          value={this.state.message}
+          onChange={(e) => {
+            this.setState({ message: e.target.value });
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            alert(this.state.message);
+            this.setState({ message: "" });
+          }}
+        >
+          enter
+        </button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
