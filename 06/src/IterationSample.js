2c61921c4f721341096329049cab31616e67466d
dasoml <somgrapik@gmail.com>
2020-08-12 21:13:57 +0900

    배열에 새로운 데이터 추가하여 리스트에 추가해보기 연습

--- 06/src/IterationSample.js
+++ 06/src/IterationSample.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
 
-  /* key pros가 없다는 오류 해결. 데이터가 가진 고유한 값이 없을때는 key 값을 index로 사용
-  게시물 번호같이 key값이 있으면 Article key = {article.id} 와 같이 넣어줘야함.
-  index를 key로 사용하면 배열이 변경될 때 효율적으로 리렌더링 하지 못한다 */
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+  //새로운 항목을 추가할때의 id : 5
 
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  const onChange = (e) => setInputText(e.target.value);
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const nameLists = names.map((name) => <li key={names.id}>{name.text}</li>);
+
   return (
-    <ul>{nameList}</ul>
-
-    /* 아래의 코드를 map 함수를 사용하여 정리*/
-    //   <ul>
-    //       <li>눈사람</li>
-    //       <li>얼음</li>
-    //       <li>눈</li>
-    //       <li>바람</li>
-    //   </ul>
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameLists}</ul>
+    </>
   );
 };
 
