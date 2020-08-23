a1329b5b2904f0329a280d7a4942b110c83d480b
dasoml <somgrapik@gmail.com>
2020-08-23 18:28:00 +0900

    CSS Selector을 사용하여 코드 수정해보기

--- 09/src/App.js
+++ 09/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <img src={logo} className="logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
