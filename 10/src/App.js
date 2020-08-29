b126d745bf0e27eb38f3c1ebe165c546bb9c4ec3
dasoml <somgrapik@gmail.com>
2020-08-29 21:05:56 +0900

    App에서 todos 상태 사용하기 (useState)

--- 10/src/App.js
+++ 10/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
-import TodoLIst from './components/TodoLIst';
+import TodoList from './components/TodoList.js';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '까망이 밥주기', checked: true },
+    { id: 2, text: '청소하기', checked: true },
+    { id: 3, text: '빨래하기', checked: false },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoLIst />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
--- 10/src/components/TodoLIst.js
+++ 10/src/components/TodoLIst.js
@@ -2,14 +2,14 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoLIst = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
 
-export default TodoLIst;
+export default TodoList;
