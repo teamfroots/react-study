4292b1edc447739824690dbbf01565b639faff20
dasoml <somgrapik@gmail.com>
2020-09-06 22:31:12 +0900

    immer를 사용하여 깔끔한 코드로 업데이트

--- 12/src/App.js
+++ 12/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from "react";
+import produce from "immer";
 
 const App = () => {
   const nextId = useRef(1);
@@ -11,12 +12,13 @@ const App = () => {
   const onChange = useCallback(
     (e) => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value],
-      });
+      setForm(
+        produce(draft => {
+          draft[name] = value;
+        })
+      );
     },
-    [form]
+    []
   );
 
   const onSubmit = useCallback(
@@ -28,10 +30,11 @@ const App = () => {
         username: form.username,
       };
 
-      setData({
-        ...data,
-        array: data.array.concat(info),
-      });
+      setData(
+        produce(draft => {
+          draft.array.push(info);
+        })
+      );
 
       setForm({
         name: "",
@@ -39,17 +42,21 @@ const App = () => {
       });
       nextId.current += 1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
   );
 
   const onRemove = useCallback(
     (id) => {
-      setData({
-        ...data,
-        array: data.array.filter((info) => info.id !== id),
-      });
+      setData(
+        produce(draft => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
     },
-    [data]
+    []
   );
 
   return (
