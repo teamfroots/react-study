7745352c5e96ad20c7fa5ae2aa10eccddcd06eb1
dasoml <somgrapik@gmail.com>
2020-10-10 17:16:29 +0900

    프로필 페이지 생성하여 url 파라미터 사용하기

--- 13/src/App.js
+++ 13/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./about";
 import Home from "./home";
+import Profile from "./Profile";
 
 const App = () => {
   return (
@@ -13,10 +14,17 @@ const App = () => {
         <li>
           <Link to="/About">ABOUT</Link>
         </li>
+        <li>
+          <Link to="/Profile/dasom">다솜의 PROFILE</Link>
+        </li>
+        <li>
+          <Link to="/Profile/kkamang">까망의 PROFILE</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
+      <Route path={"/profile/:username"} component={Profile} />
     </div>
   );
 };
